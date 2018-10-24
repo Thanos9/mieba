ac7b57e2d50313fc7650d561aaede73aa9e243bd
thanos <1309973953@qq.com>
2018-10-24 03:15:01 -0400

    20181024

--- hardseedcmd.sh
+++ hardseedcmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #!/bin/sh
-nohup /usr/local/bin/hardseed --saveas-path ~/downloadsTmp --topics-range 32 --proxy "" > ~/nohup.log 2>&1 &
+nohup /usr/local/bin/hardseed --saveas-path ~/downloadsTmp --topics-range 256 --like むしゅうせい 步兵 無碼 無修正  --proxy "" > ~/nohup.log 2>&1 &
